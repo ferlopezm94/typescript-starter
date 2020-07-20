2e35bccc935b58059582c3a69a87600beef11f63
Fernando López <ferlopezm94@gmail.com>
2020-07-19 22:14:06 -0500

    chore(jest): add node test environment

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.(e2e|unit).test.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  testMatch: ['**/__tests__/**/*.(e2e|unit).test.ts'],
 };
